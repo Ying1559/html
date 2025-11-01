80cdef44b27a6638ec2ba216653bc4bfdc18c75c
Ying1559 <jeancherry.chen@gmail.com>
2025-11-02 01:49:55 +0800

    Delete 15.login.php

--- 15.login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ($_POST["id"]=="tony" && $_POST["pwd"]=="1234")
-        echo "login success";
-    else
-        echo "login fail";
- <!--
-  使用 $_POST 取得從表單傳來的資料，id" 與 "pwd" 對應到表單 <input name="id">、<input name="pwd">，判斷帳號與密碼是否正確
-  -->
-?>
